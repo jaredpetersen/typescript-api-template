ff4f36c1c77eae1e07a7e31250adfbf94bc29c10
Jared Petersen <jaredtoddpetersen@gmail.com>
2019-03-01 23:47:39 -0800

    fixed travis ci

--- src/middleware/index.test-ut.ts
+++ src/middleware/index.test-ut.ts
@@ -1,8 +1,8 @@
 'use strict';
 
+import logger from '../logger';
 import * as middleware from './index';
 import { Request, Response, NextFunction } from 'express';
-import logger from '../logger';
 
 jest.mock('../logger');
 
@@ -11,16 +11,10 @@ describe('Middleware - UT', () => {
   describe('doSomethingInteresting()', () => {
 
     test('logs something', () => {
-      // Stub req
       const reqStub: Partial<Request> = {};
-
-      // Mock res
       const resMock: Partial<Response> = {};
-
-      // Stub next
       const nextStub: NextFunction = () => {};
 
-      // Run unit under test
       middleware.doSomethingInteresting(<Request>reqStub, <Response>resMock, nextStub);
 
       expect(logger.log).toHaveBeenCalledWith('info', 'interesting middleware');
