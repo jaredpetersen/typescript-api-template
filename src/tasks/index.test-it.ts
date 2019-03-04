292b2fc1b1b24c69f86f541b472ec1792cde3072
Jared Petersen <jaredtoddpetersen@gmail.com>
2019-03-03 17:40:19 -0800

    changed line width to 100 characters

--- src/tasks/index.test-it.ts
+++ src/tasks/index.test-it.ts
@@ -9,7 +9,11 @@ describe('Tasks - IT', () => {
       const result = await request(app).get('/tasks');
 
       expect(result.status).toEqual(200);
-      expect(result.body).toEqual([{ _id: 1, name: 'milk' }, { _id: 2, name: 'cheese' }, { _id: 3, name: 'bread' }]);
+      expect(result.body).toEqual([
+        { _id: 1, name: 'milk' },
+        { _id: 2, name: 'cheese' },
+        { _id: 3, name: 'bread' }
+      ]);
     });
   });
 
